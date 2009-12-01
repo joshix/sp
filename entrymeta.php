eb0f314ad058b8f8033a6997d636f792cadf35d5
utopian labs <utopian.labs@gmail.com>
2009-12-01 02:52:40 -0800

    Habari code style: space amidst if (). CSS comment style (and clarity).  See issue #5.

--- entrymeta.php
+++ entrymeta.php
@@ -1,16 +1,16 @@
 					<ul class="entry-meta">
 
-					<?php if( $post->tags ) : ?>
+					<?php if ( $post->tags ) : ?>
 						<li class="entry-tags"><?php _e( 'Tags:', 'sp' ); ?> <?php echo $post->tags_out; ?></li>
 					<?php endif; ?>
 
-					<?php if( !$post->info->comments_disabled || $post->comments->approved->count > 0 ) : ?>
+					<?php if ( !$post->info->comments_disabled || $post->comments->approved->count > 0 ) : ?>
 						<li class="commentslink">
 							<a href="<?php echo $post->permalink; ?>#comments" title="<?php _e('Comments on this post'); ?>"><?php echo $post->comments->approved->count; ?> <?php echo _n( 'Comment', 'Comments', $post->comments->approved->count, 'sp' ); ?></a> &ndash; <a href="<?php echo $post->comment_feed_link; ?>"><?php _e( 'Feed', 'sp' ); ?></a>
 						</li>
 					<?php endif; ?>
 
-					<?php if( $loggedin ) : ?>
+					<?php if ( $loggedin ) : ?>
 						<li><a href="<?php echo $post->editlink; ?>" title="<?php _e( 'Edit post', 'sp' ); ?>"><?php _e( 'Edit', 'sp' ); ?></a></li>
 					<?php endif; ?>
 
