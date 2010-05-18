7f69a72ac6392621e72277042af595d05142678f
Josh Wood <josh@utopian.net>
2010-05-18 02:10:19 -0700

    Eliminate manual/direct Page menu code in sidebar.
    Don't manually build a page menu, or directly check for
    pagemenu plugin, in sidebar.
    
    For page menu, use the pagemenu plugin and Blocks
    settings in admin->Themes. Minimally style a copy of
    block.pagemenu.php in the theme folder to reproduce old output.

--- sidebar.php
+++ sidebar.php
@@ -2,18 +2,6 @@
 
 	<?php $theme->display ( 'searchform' ); ?>
 
-	<?php if ( count( $pages ) > 0) : /* Requires support in $theme->add_template_vars(). */ ?>
-		<h2 id="pagelist-title"><?php _e( 'Pages', 'sp' ); ?></h2>
-		<ul id="pagelist">
-		<?php if ( Plugins::is_loaded( 'Page Menu' ) ) :
-			$theme->pagemenu();
-		else :
-			foreach( $pages as $page )
-				echo '<li><a href="' . $page->permalink . '" title="' . $page->title . '">' . $page->title . '</a></li>' . "\n";
-		endif; ?>
-		</ul>
-	<?php endif; ?>
-
 	<?php $theme->area( 'sidebar' ); ?>
 
 	<h3 id="rss"><a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>"><?php _e( 'Syndicate (Atom)', 'sp' ); ?></a></h3>
