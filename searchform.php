f668a2404c7bc91fb019a4b40efa40b63e85a922
Josh Wood <josh@utopian.net>
2011-04-02 01:12:07 -0700

    Drop searchform.
    A search form block is now (Habari 0.7+) supplied in
    Core Blocks stock plugin.

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<!-- searchform -->
-<?php Plugins::act( 'theme_searchform_before' ); ?>
-	<form method="get" id="searchform" action="<?php URL::out('display_search'); ?>">
-		<p><input type="text" id="s" name="criteria" value="<?php if ( isset ( $criteria ) ) echo htmlentities( $criteria, ENT_COMPAT, 'UTF-8' ); ?>"><br> <button type="submit" id="searchsubmit" class="submit button" value="<?php _e( 'Search', 'sp' ); ?>"><?php _e( 'Search', 'sp' ); ?></button></p>
-	</form><br class="clear">
-<?php Plugins::act( 'theme_searchform_after' ); ?>
-<!-- /searchform -->
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,5 @@
 	<div id="secondarycontent">
 
-	<?php $theme->display ( 'searchform' ); ?>
-
 	<?php $theme->area( 'sidebar' ); ?>
 
 	<h3 id="rss"><a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>"><?php _e( 'Syndicate (Atom)', 'sp' ); ?></a></h3>
