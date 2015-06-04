c14e40624c4e1664ce828327259a8214a9b7667a
j <j@joshix.com>
2015-06-04 09:44:39 +0000

    Typo oops. Add parens as per patch.

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 	<div id="secondarycontent">
 
-	<?php echo $theme->area( 'sidebar' ); ?>
+	<?php echo( $theme->area( 'sidebar' ) ); ?>
 
 	<h3 id="rss"><a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>"><?php _e( 'Syndicate (Atom)', 'sp' ); ?></a></h3>
 
